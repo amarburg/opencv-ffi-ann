16f5da0177271fd9e1ce8baac5887c4c0d85d251
Aaron Marburg <aaron.marburg@pg.canterbury.ac.nz>
2014-02-07 10:12:22 +1300

    Added OpenCV ANN code and other ephemera.

---
+++ lib/opencv/ffi/ann.rb
@@ -0,0 +1,9 @@
+require "opencv/ffi/ann/version"
+
+module Opencv
+  module Ffi
+    module Ann
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/opencv/ffi/ann/version.rb
@@ -0,0 +1,7 @@
+module Opencv
+  module Ffi
+    module Ann
+      VERSION = "0.0.1"
+    end
+  end
+end
